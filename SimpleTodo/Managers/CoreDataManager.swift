96baf016f549a42a728a8485ffba1627d8498aa5
Kelvin Kioko <kelvinmkioko@gmail.com>
2022-10-20 17:31:29 +0300

    Implemented a simple todo app in Swift UI and Core Data

---
+++ SimpleTodo/Managers/CoreDataManager.swift
@@ -0,0 +1,25 @@
+//
+//  CoreDataManager.swift
+//  SimpleTodo
+// Responsible for setting up our Core data stack
+//
+//  Created by Kelvin Kioko on 20/10/2022.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataManager {
+    
+    let persistentContainer: NSPersistentContainer
+    static let shared: CoreDataManager = CoreDataManager()
+    
+    private init() {
+        persistentContainer = NSPersistentContainer(name: "SimpleTodoModel")
+        persistentContainer.loadPersistentStores { description, error in
+            if let error = error {
+                fatalError("Unable to initialize Core Data \(error)")
+            }
+        }
+    }
+}
